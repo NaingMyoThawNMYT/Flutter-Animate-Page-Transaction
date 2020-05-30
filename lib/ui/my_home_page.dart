6a99c28982cbe8af80acc055fcc07049efd160ed
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2020-05-30 11:56:29 +0630

    2. "SlideTransition" animation is added to go to the second page.

--- lib/ui/my_home_page.dart
+++ lib/ui/my_home_page.dart
@@ -11,7 +11,61 @@ class MyHomePage extends StatelessWidget {
         title: Text(title),
       ),
       body: Center(
-        child: Text('Flutter Animate Page Transaction'),
+        child: RaisedButton(
+          child: Text('Slide Transition'),
+          onPressed: () {
+            Navigator.push(context, _createRoute());
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      // Offset(0.0, 0.0) // replace
+      // Offset(0.0, 1.0) // come from bottom
+      // Offset(1.0, 0.0) // come from right
+      // Offset(1.0, 1.0) // come from right|bottom
+      var begin = Offset(1.0, 0.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+      var curveTween = CurveTween(curve: curve);
+      var tween = Tween(begin: begin, end: end).chain(curveTween);
+      var offsetAnimation = animation.drive(tween);
+
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
+    },
+
+//    using CurvedAnimation
+//    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+//      var begin = Offset(0.0, 1.0);
+//      var end = Offset.zero;
+//      var tween = Tween(begin: begin, end: end);
+//      var curve = Curves.ease;
+//      var curvedAnimation = CurvedAnimation(parent: animation, curve: curve);
+//
+//      return SlideTransition(
+//        position: tween.animate(curvedAnimation),
+//        child: child,
+//      );
+//    },
+  );
+}
+
+class Page2 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.blue,
+        child: Center(child: Text('Page 2')),
       ),
     );
   }
